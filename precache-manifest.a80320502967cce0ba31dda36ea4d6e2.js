d4fb6fec67dd207185e7524b7999fd4246ab3171
Ragulan Kodeeswaran <ragulankodeeswaran@Ragulans-MacBook-Pro.local>
2020-01-21 22:41:44 -0500

    Updates

---
+++ precache-manifest.a80320502967cce0ba31dda36ea4d6e2.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "11572abcda96d1a00be1e1f300fb99bb",
+    "url": "/index.html"
+  },
+  {
+    "revision": "3048df1e51f14951c5d6",
+    "url": "/static/css/2.17e5ed98.chunk.css"
+  },
+  {
+    "revision": "ec76c856c50d45fbaedf",
+    "url": "/static/css/main.0ead5a3b.chunk.css"
+  },
+  {
+    "revision": "3048df1e51f14951c5d6",
+    "url": "/static/js/2.0db038fd.chunk.js"
+  },
+  {
+    "revision": "2f6560220ce59751f007cbfc0b04c53d",
+    "url": "/static/js/2.0db038fd.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ec76c856c50d45fbaedf",
+    "url": "/static/js/main.a6d59413.chunk.js"
+  },
+  {
+    "revision": "bfa0ebfd2ee9410f8a8a",
+    "url": "/static/js/runtime-main.dae0cf05.js"
+  },
+  {
+    "revision": "3a5419e386d463ec60bec670c79dee48",
+    "url": "/static/media/calorify.3a5419e3.jpeg"
+  },
+  {
+    "revision": "d13b8f680f221f2655606c0885d3ed07",
+    "url": "/static/media/hiragu.d13b8f68.png"
+  },
+  {
+    "revision": "ddc218fc53b7af197df9461015cf9b00",
+    "url": "/static/media/logo2.ddc218fc.png"
+  },
+  {
+    "revision": "6ed6feae6d43cc168927fbd08697f2d7",
+    "url": "/static/media/q-eco-vechicle.6ed6feae.jpg"
+  },
+  {
+    "revision": "1ad05d10de32caaa45aab3e4fb2e12d0",
+    "url": "/static/media/ragulan.1ad05d10.jpg"
+  }
+]);
